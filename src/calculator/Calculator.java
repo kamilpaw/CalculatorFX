5dd9230c4e4547fae7ed4a04107bc3284fd0e664
unknown <kamilpawlowski.mail@gmail.com>
2021-07-09 11:08:05 +0200

    Changing Decimals to BigDecimals

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -3,6 +3,7 @@ package calculator;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
@@ -10,12 +11,13 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Calculator extends Application {
 
     String display = "";
     String operator = "";
-    Double temp = 0.0;
+    BigDecimal temp = new BigDecimal(0.0);
 
     public void start(Stage stage) {
         VBox vBox = new VBox();
@@ -31,7 +33,7 @@ public class Calculator extends Application {
         String s = "789/456x123-0.=+";
 
         for (int i = 0; i < s.length(); i++) {
-            javafx.scene.control.Button button = new javafx.scene.control.Button(String.valueOf(s.charAt(i)));
+            Button button = new Button(String.valueOf(s.charAt(i)));
             button.setPrefSize(100, 100);
             button.setFont(new Font(45));
             button.setOnAction((event) -> {
@@ -40,60 +42,58 @@ public class Calculator extends Application {
 
                 if (input.matches("0|1|2|3|4|5|6|7|8|9") || input.equals(".")) {
                     display = display + input;
-
                 }
 
                 if (input.equals("+") || input.matches("-|/|x")) {
 
-                    if (!display.isEmpty() && temp == 0) {
-                        temp = Double.valueOf(display);
+                    if (!display.isEmpty() && temp.compareTo(BigDecimal.ZERO)==0) {
+                        temp = new BigDecimal(display);
                     }
 
 
                     if (!operator.isEmpty() && !display.isEmpty()) {
                         if (operator.equals("+")) {
-                            temp = temp + Double.valueOf(display);
+                            temp = temp.add(new BigDecimal(display));
                         }
                         if (operator.equals("-")) {
-                            temp = temp - Double.valueOf(display);
+                            temp = temp.subtract(new BigDecimal(display));
                         }
                         if (operator.equals("x")) {
-                            temp = temp * Double.valueOf(display);
+                            temp = temp.multiply(new BigDecimal(display));
                         }
                         if (operator.equals("/")) {
                             if (!(Double.valueOf(display) == 0)) {
-                                temp = temp / Double.valueOf(display);
+                                temp = temp.divide(new BigDecimal(display), RoundingMode.HALF_UP);
                             }
                         }
                     }
                     operator = input;
-
                     display = "";
                 }
 
                 if (input.equals("=") && !operator.isEmpty()) {
                     if (operator.equals("+")) {
-                        temp = temp + Double.valueOf(display);
+                        temp = temp.add(new BigDecimal(display));
                     }
                     if (operator.equals("-")) {
-                        temp = temp - Double.valueOf(display);
+                        temp = temp.subtract(new BigDecimal(display));
                     }
                     if (operator.equals("x")) {
-                        temp = temp * Double.valueOf(display);
+                        temp = temp.multiply(new BigDecimal(display));
                     }
                     if (operator.equals("/")) {
                         if (!(Double.valueOf(display) == 0)) {
-                            temp = temp / Double.valueOf(display);
+                            temp = temp.divide(new BigDecimal(display),6, RoundingMode.HALF_DOWN);
                         }
                     }
                     operator = "";
-                    BigDecimal bd = new BigDecimal(temp);
-                    display = String.valueOf(bd);
+                    temp = temp.stripTrailingZeros();
+                    display = String.valueOf(temp);
                 }
                 displayArea.setText(display);
-                System.out.println(temp);
-                System.out.println(operator);
-                System.out.println(display);
+                System.out.println("temp: " + temp);
+                System.out.println("operator: " + operator);
+                System.out.println("display: " + display);
 
 
             });
